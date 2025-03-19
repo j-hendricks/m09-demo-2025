5146a0c3e87a7b35998ab2b4859b87ae67f80a8e
JohnWilliam_Hendricks <jwh8zc@udc-ba37-32c1.(none)>
2025-03-18 20:05:09 -0400

    Created initial module

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='Demo',
+	version='0.1',
+	description="lol",
+	url="https://github.com/j-hendricks/m09-demo-2025",
+	author="jwh8zc",
+	author_email="jwh8zc@virginia.edu",
+	license='MIT',
+	packages=['demo']
+)
